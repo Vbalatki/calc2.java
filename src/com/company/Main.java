ddf66d5f7202439bb07cd88cd80a7862369b9e5e
Vbalatki <154458671+Vbalatki@users.noreply.github.com>
2024-02-07 17:44:32 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,74 +1,66 @@
 package com.company;
 
-import java.io.IOException;
-import java.lang.*;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class Main {
-    static int n, m;//значения
-    static String a = "  ", b = "  "; //переходные значения n & m
-    static String operation = " "; //арифметическая операция
-    static boolean bool = true, n1, m1; //клеймо n & m арабские или римские (false = арабские)
-    static Scanner scanner = new Scanner(System.in);
-
-
-    public static void main(String[] args) {
-
+class Main  {
+    public static void main(String[] args) throws InputMismatchException {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Введи выражение: ");
         String input = scanner.nextLine(); //считываем выражение
+        String a = " ";
+        try {
+            a = Calc.calc(input);
+        }catch (InputMismatchException e){
+            System.out.print(e.getMessage());
+            System.exit(0);
+        }
+        System.out.println(a);
+    }
+}
+public class Calc {
+    static int n, m;//значения
+    static String a, b; //переходные значения n & m
+    static String operation = " "; //арифметическая операция
+    static boolean n1 = false, m1 = false; //клеймо n & m арабские или римские (false = арабские)
+    static Scanner scanner = new Scanner(System.in);
 
-        Operation(input);
 
-        a = RimToArab(a);
-        n1 = bool;
-        bool = true;
-        b = RimToArab(b);
-        m1 = bool;
+    public static String calc(String input) throws InputMismatchException{
 
+        Operation(input); //разбивает числа и символы в отдельные поля
 
-        // исключение для услвоия 4
-        n =  IntegerCalcException(a);
-        m =  IntegerCalcException(b);
+        //проверка на алфавит символов
+        String[] Rim = {"1", "2", "3", "4",  "5", "6", "7", "8", "9", "10"};
+        for (int i = 0; i < 10; i++){
+            if (a.equals(Rim[i])) n1 = true;
+            if (b.equals(Rim[i])) m1 = true;
+        }
+        if (n1 != m1) throw new InputMismatchException("Некорректное выражение");
 
-        // исключение для условия 3
-        RangeCalcException(n, m);
+        m = RimToArab(a);
+        n = RimToArab(b);
 
-        // исключение для условия 5
-        RimArabCalcException(n1, m1);
 
         //переводим все в одну систему
-        String result = calc(operation, n, m);
+        String result = Symbol(operation, m, n);
         int resultt = Integer.parseInt(result);
-        if (bool == true) result = ArabToRim(resultt);
+        if (n1 != true) result = ArabToRim(resultt);
 
 
-        System.out.print(result);
+        return result;
     }
 
 
-    public static String calc(String operation, int first, int second) {
-        switch (operation) {
-            case "+":
-                return Integer.toString(first + second);
-            case "-":
-                return Integer.toString(first - second);
-            case "*":
-                return Integer.toString(first * second);
-            case "/":
-                return Integer.toString(first / second);
-        }
-        return " ";
-    }
-
-
-    private static void Operation(String input) {
+    private static void Operation(String input) throws InputMismatchException{
         //определение значения и операции
 
-        char symbol; //переходное значение operation
+        char symbol = ' '; //переходное значение operation
         for (int i = 0; i < input.length(); i++) {
             symbol = input.charAt(i);
+
+            if (symbol == '.') throw new InputMismatchException("Введите целые числа");
             if (symbol == '+' || symbol == '-' || symbol == '*' || symbol == '/') {
 
                 if (symbol == '+') operation = "+";
@@ -81,86 +73,56 @@ public class Main {
             }
 
         }
+        if (operation == " ") throw new InputMismatchException("Некорректное выражение");
     }
 
-    private static String RimToArab(String Rim) {
+    private static int RimToArab(String Rim) throws InputMismatchException {
         switch (Rim) {
-            case "I":
-                return "1";
-            case "II":
-                return "2";
-            case "III":
-                return "3";
-            case "IV":
-                return "4";
-            case "V":
-                return "5";
-            case "VI":
-                return "6";
-            case "VII":
-                return "7";
-            case "VIII":
-                return "8";
-            case "IX":
-                return "9";
-            case "X":
-                return "10";
+            case "I", "1":
+                return 1;
+            case "II", "2":
+                return 2;
+            case "III", "3":
+                return 3;
+            case "IV", "4":
+                return 4;
+            case "V", "5":
+                return 5;
+            case "VI", "6":
+                return 6;
+            case "VII", "7":
+                return 7;
+            case "VIII", "8":
+                return 8;
+            case "IX", "9":
+                return 9;
+            case "X", "10":
+                return 10;
+            default:
+                throw new InputMismatchException("Некорректное выражение");
         }
-        bool = false;
-        return Rim;
+    }
+
+    private static String Symbol(String operation, int first, int second) throws InputMismatchException{
+        switch (operation) {
+            case "+":
+                return Integer.toString(first + second);
+            case "-":
+                if (n1 == false && first - second < 1) throw new InputMismatchException("Результат работы с римскими числами не может быть меньше единицы");
+
+                else return Integer.toString(first - second);
+            case "*":
+                return Integer.toString(first * second);
+            case "/":
+                return Integer.toString(first / second);
+        }
+        return " ";
     }
 
 
     private static String ArabToRim(int result) {
-        String[] Rim = {"O", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX", "XXI", "XXII", "XXIII", "XXIV", "XXV", "XXVI", "XXVII", "XXVIII", "XXIX", "XXX", "XXXI", "XXXII", "XXXIII", "XXXIV", "XXXV", "XXXVI", "XXXVII", "XXXVIII", "XXXIX", "XL",  "XLI", "XLII", "XLIII", "XLIV", "XLV", "XLVI", "XLVII", "XLVIII", "XLIX", "L", "LI", "LII", "LIII", "LIV", "LV", "LVI", "LVII", "LVIII", "LIX", "LX",  "LXI", "LXII", "LXIII", "LXIV", "LXV", "LXVI", "LXVII", "LXVIII", "LXIX", "LXX",  "LXXI", "LXXII", "LXXIII", "LXXIV", "LXXV", "LXXVI", "LXXVII", "LXXVIII", "LXXIX", "LXXX", "LXXXI", "LXXXII", "LXXXIII", "LXXXIV", "LXXXV", "LXXXVI", "LXXXVII", "LXXXVIII", "LXXXIX", "XC",  "XCI", "XCII", "XCIII", "XCIV", "XCV", "XCVI", "XCVII", "XCVIII", "XCIX", "C" };
+        String[] Rim = {"O", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX", "XXI", "XXII", "XXIII", "XXIV", "XXV", "XXVI", "XXVII", "XXVIII", "XXIX", "XXX", "XXXI", "XXXII", "XXXIII", "XXXIV", "XXXV", "XXXVI", "XXXVII", "XXXVIII", "XXXIX", "XL", "XLI", "XLII", "XLIII", "XLIV", "XLV", "XLVI", "XLVII", "XLVIII", "XLIX", "L", "LI", "LII", "LIII", "LIV", "LV", "LVI", "LVII", "LVIII", "LIX", "LX", "LXI", "LXII", "LXIII", "LXIV", "LXV", "LXVI", "LXVII", "LXVIII", "LXIX", "LXX", "LXXI", "LXXII", "LXXIII", "LXXIV", "LXXV", "LXXVI", "LXXVII", "LXXVIII", "LXXIX", "LXXX", "LXXXI", "LXXXII", "LXXXIII", "LXXXIV", "LXXXV", "LXXXVI", "LXXXVII", "LXXXVIII", "LXXXIX", "XC", "XCI", "XCII", "XCIII", "XCIV", "XCV", "XCVI", "XCVII", "XCVIII", "XCIX", "C"};
         String s = Rim[result];
         return s;
     }
-
-    // все исключения
-
-
-    private static void RangeCalcException(int a, int b) {
-        try {
-            if (1 > a || a > 10 || 1 > b || b > 10) throw new Exception("Введите числа от 1 до 10");
-        } catch (Exception ex) {
-            System.out.println("Exception" + "\n" + ex.getMessage());
-            System.exit(0); // возвращаем 0 в случае возникновения исключения
-        }
-    }
-
-
-    private static int IntegerCalcException(String b) throws NumberFormatException {
-
-        double a;
-        try {
-            a = Double.parseDouble(b);
-        } catch (NumberFormatException ex) {
-            System.out.println("Exception");
-            System.exit(0);
-        }
-
-        a = Double.parseDouble(b);
-        try {
-            if (a % 1 != 0) throw new NumberFormatException("Введите целые числа");
-        } catch (NumberFormatException ex) {
-            System.out.println("Exception" + "\n" + ex.getMessage());
-            System.exit(0);
-        }
-        return (int) a;
-    }
-
-
-    private static void RimArabCalcException(boolean a, boolean b ){
-        try {
-            if (a != b) throw new Exception("Введите числа одного типа");
-            if (bool == true && n < m) throw new Exception("Результат не может быть отрицательный, \nкогда используются римские цифры");
-        }
-        catch (Exception ex){
-            System.out.println("Exception" + "\n" + ex.getMessage());
-            System.exit(0);
-        }
-    }
-
-
 }
